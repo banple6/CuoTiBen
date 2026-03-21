5a8d4a9eb834ab788dcbaba3ad143d76a43b1187
TianBoyu <bjtby@icloud.com>
2026-03-21 12:52:11 +0800

    Initial Commit

---
+++ CuoTiBen/CuoTiBenApp.swift
@@ -0,0 +1,17 @@
+//
+//  CuoTiBenApp.swift
+//  CuoTiBen
+//
+//  Created by 田博雨 on 2026/3/21.
+//
+
+import SwiftUI
+
+@main
+struct CuoTiBenApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
